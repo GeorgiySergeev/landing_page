7c6820561f13f96f0adcd0afa4206d84193247ac
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-04-21 13:54:28 +0300

    test buld

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const config = {
-	mode: 'production',
-	entry: {
-		index: './src/js/index.js'
-		// contacts: './src/js/contacts.js',
-		// about: './src/js/about.js',
-	},
-	output: {
-		filename: '[name].bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  mode: 'production',
+  entry: {
+    index: './src/js/index.js',
+    // contacts: './src/js/contacts.js',
+    // about: './src/js/about.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 8080, // Указываем порт 8080
+    // Другие настройки сервера, если необходимо
+  },
 };
 
 module.exports = config;
