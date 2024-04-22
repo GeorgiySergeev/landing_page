857323a05b78dcbf4e120b3804fbdd9204010063
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-04-22 21:18:13 +0300

    version

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,14 @@ import { Fancybox } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+  // Your custom options
 });
 
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+import timer from './modules/timer';
+
+// const targetDate = new Date('2024-04-22T00:00:00'); // Установка целевой даты
+// const currentDate = new Date(); // Получение текущей даты
+// const remainingTime = targetDate - currentDate; // Вычисление оставшегося времени
---
+++ src/js/modules/timer.js
@@ -0,0 +1,44 @@
+// const dayEl = document.querySelector('[data-days]');
+// const hoursEl = document.querySelector('[data-hours]');
+// const minEl = document.querySelector('[data-minutes]');
+
+// function convertMs() {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   return { day, hour, minute, second };
+// }
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+// function timer(ms) {
+//   const { day, hour, minute, second } = convertMs(ms);
+
+//   dayEl.textContent = addLeadingZero(Math.floor(ms / day));
+//   hoursEl.textContent = addLeadingZero(Math.floor((ms % day) / hour));
+//   minEl.textContent = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+// }
+function timer() {
+  const targetDate = new Date('2024-04-23T00:00:00');
+  const currentDate = new Date();
+
+  const totalSeconds = Math.floor((targetDate - currentDate) / 1000);
+  const days = Math.floor(totalSeconds / (3600 * 24));
+  const hours = Math.floor((totalSeconds % (3600 * 24)) / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+
+  const daysEl = document.querySelector('[data-days]');
+  const hoursEl = document.querySelector('[data-hours]');
+  const minutesEl = document.querySelector('[data-minutes]');
+
+  daysEl.textContent = days;
+  hoursEl.textContent = hours < 10 ? '0' + hours : hours;
+  minutesEl.textContent = minutes < 10 ? '0' + minutes : minutes;
+}
+
+setInterval(timer, 1000);
+export default timer;
